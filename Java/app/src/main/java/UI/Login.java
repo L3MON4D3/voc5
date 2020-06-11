1ab6c6e1cf73332af825c42d02c25c4928bfea76
L3MON4D3 <Simljk@outlook.de>
2020-06-11 19:25:34 +0200

    Add dummy-Login Function.

--- Java/app/src/main/java/UI/Login.java
+++ Java/app/src/main/java/UI/Login.java
@@ -1,14 +1,54 @@
 package com.L3MON4D3.voc5;
 
-import android.os.Bundle;
+import okhttp3.OkHttpClient;
+import okhttp3.Callback;
+import okhttp3.Call;
+import okhttp3.Response;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import android.util.Log;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
+import java.io.IOException;
 
 public class Login extends AppCompatActivity {
+    Voc5Client vClient;
+    OkHttpClient okClient = new OkHttpClient();
+
+    EditText emailET;
+    EditText pwdET;
+
+    View.OnClickListener loginListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            vClient = new Voc5Client(
+                emailET.getText().toString(), pwdET.getText().toString());
+            okClient.newCall(vClient.loginRqst()).enqueue(loginCallback);
+        }
+    };
+
+    Callback loginCallback = new Callback() {
+        public void onFailure(Call call, IOException e) {
+            Log.e("voc5:", "not successful:"+e.getMessage());
+        }
+
+        public void onResponse(Call call, Response res) throws IOException {
+            Log.e("voc5:", res.body().string());
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        emailET = findViewById(R.id.usr_et);
+        pwdET = findViewById(R.id.pwd_et);
+
+        findViewById(R.id.login_btn).setOnClickListener(loginListener);
     }
 }
