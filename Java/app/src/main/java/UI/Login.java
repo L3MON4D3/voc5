ed49986cdf3998892b3c5eac16c14a046c310a62
L3MON4D3 <Simljk@outlook.de>
2020-06-11 18:23:14 +0200

    Change MainActivity to Login.

--- Java/app/src/main/java/UI/Login.java
+++ Java/app/src/main/java/UI/Login.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
