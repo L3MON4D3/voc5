2c44be8cb05e9fdd59da97e02ab1a6cfed8a5d44
L3MON4D3 <Simljk@outlook.de>
2020-06-11 12:41:54 +0200

    Init Android-Project.

---
+++ Java/app/src/main/java/UI/MainActivity.java
@@ -0,0 +1,14 @@
+package com.L3MON4D3.voc5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
