79c4eb005b26bf31c4608001b829eef5be552a11
L3MON4D3 <Simljk@outlook.de>
2020-06-20 14:57:59 +0200

    Add Gallery class, first working example.

---
+++ Java/app/src/main/java/UI/GalleryActivity.java
@@ -0,0 +1,12 @@
+package com.L3MON4D3.voc5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GalleryActivity extends VocActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+    }
+}
