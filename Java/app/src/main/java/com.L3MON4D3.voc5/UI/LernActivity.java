452db0468fa3dc1b48ef9b6771cb95507f232c65
L3MON4D3 <Simljk@outlook.de>
2020-07-10 01:59:22 +0200

    TextView in lern_activity2 submits on Enter.

--- Java/app/src/main/java/com.L3MON4D3.voc5/UI/LernActivity.java
+++ Java/app/src/main/java/com.L3MON4D3.voc5/UI/LernActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import java.util.Random;
 import java.util.ArrayList;
 import com.L3MON4D3.voc5.Client.Vocab;
@@ -19,17 +20,24 @@ import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 
+import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.KeyEvent;
+import android.app.Activity;
+
 public class LernActivity extends VocActivity {
+    private static final int POPREQ=11;
+
     Button checkbtn;
     Button finishbtn;
     Random rad = new Random();
     Vocab currentVoc;
-    private static final int POPREQ=11;
     ArrayList<Vocab> vocs;
     IntPair[] newPhases;
     FrameLayout cardParent;
     GalleryCardFactory gcf;
     EditText editTextAnswer;
+    private InputMethodManager imm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,8 @@ public class LernActivity extends VocActivity {
         editTextAnswer = (EditText) findViewById(R.id.editTextAnswer);
         finishbtn =(Button) findViewById(R.id.finishbtn);
         checkbtn = findViewById(R.id.checkbtn);
+
+        imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
 
         //Code from GalleryActivity to determine width of cards.
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -83,23 +93,24 @@ public class LernActivity extends VocActivity {
         checkbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String currentAns = editTextAnswer.getText().toString();
-                //correct answer was given, popup does not start, new currentVoc is selected
-                if (currentAns.equals(currentVoc.getAnswer())) {
-                        currentVoc.incPhase();
-                        saveChanges(currentVoc);
-                        tolern();
-                } else {
-                    //Popup is started, userAnswer,rigthAnswer, and the phase of currentVoc is passed
-                        Intent startIntent = new Intent(getApplicationContext(), Pop.class);
-                        startIntent.putExtra("userAnswer", editTextAnswer.getText().toString());
-                        startIntent.putExtra("rightAnswer", currentVoc.getAnswer());
-                        startIntent.putExtra("phase", currentVoc.getPhase());
-                        startActivityForResult(startIntent, POPREQ);
-                }
+                answerCheck();
             }
         });
+
+        editTextAnswer.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    answerCheck();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
     }
+
     /**
      * tolern() chooses a currentVoc and calls window()
      * if ArrayList vocs is empty, the GalleryActivity will start
@@ -116,6 +127,26 @@ public class LernActivity extends VocActivity {
             setResult(RESULT_OK, startIntent);
             finish();
         }
+    }
+
+    /**
+     * Check Users Answer and Start PopActivity or directly load next Vocab.
+     */
+    public void answerCheck() {
+            String currentAns = editTextAnswer.getText().toString();
+            //correct answer was given, popup does not start, new currentVoc is selected
+            if (currentAns.equals(currentVoc.getAnswer())) {
+                    currentVoc.incPhase();
+                    saveChanges(currentVoc);
+                    tolern();
+            } else {
+                //Popup is started, userAnswer,rigthAnswer, and the phase of currentVoc is passed
+                    Intent startIntent = new Intent(getApplicationContext(), Pop.class);
+                    startIntent.putExtra("userAnswer", editTextAnswer.getText().toString());
+                    startIntent.putExtra("rightAnswer", currentVoc.getAnswer());
+                    startIntent.putExtra("phase", currentVoc.getPhase());
+                    startActivityForResult(startIntent, POPREQ);
+            }
     }
     /**
      * window() shows the current Question on Screen
